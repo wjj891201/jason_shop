f384e200379cf165b27c5e0642f2e3b9d4cbe2b2
wujiepeng <517987404@qq.com>
2019-06-04 09:20:54 +0800

    first commit

---
+++ config/db.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=127.0.0.1;dbname=jason_shop',
+    'username' => 'root',
+    'password' => 'root',
+    'charset' => 'utf8',
+    'tablePrefix' => 'shop_',
+];
---
+++ modules/controllers/PublicController.php
@@ -0,0 +1,49 @@
+<?php
+namespace app\modules\controllers;
+
+use yii\web\Controller;
+use app\modules\models\Admin;
+use Yii;
+
+class PublicController extends Controller
+{
+    public function actionLogin()
+    {
+        $this->layout = false;
+        $model = new Admin;
+        if (Yii::$app->request->isPost) {
+            $post = Yii::$app->request->post();
+            if ($model->login($post)) {
+                $this->redirect(['default/index']);
+                Yii::$app->end();//结束
+            }
+        }
+        return $this->render("login", ['model' => $model]);
+    }
+
+    public function actionLogout()
+    {
+        Yii::$app->session->removeAll();
+        if (!isset(Yii::$app->session['admin']['isLogin'])) {
+            $this->redirect(['public/login']);
+            Yii::$app->end();
+        }
+        $this->goback();//从哪个页面过的再跳回哪个页面
+    }
+
+    public function actionSeekpassword()
+    {
+        $this->layout = false;
+        $model = new Admin;
+        if (Yii::$app->request->isPost) {
+            $post = Yii::$app->request->post();
+            if ($model->seekPass($post)) {
+                Yii::$app->session->setFlash('info', '电子邮件已经发送成功，请查收');
+            }
+        }
+        return $this->render("seekpassword", ['model' => $model]);
+    }
+
+
+
+}
